c7324ef2ad7fe8f5d25ee98f3b28f73ee8f3fa8a
Samu20060127 <ferenczi.samuel2006@gmail.com>
2021-09-24 15:39:28 +0200

    change again

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ function help(message) {
   const replymsg = `**__Feri help__ **
   *commands:*
   - **!play**
-     !play + *songs name* => the bot will join to your voice channel and plays the song
+     !play + *songs name* => the bot is going to join to your voice channel and play the song
   -**!skip**
      skips the songs and go to the next one on the queue
   -**!stop**
